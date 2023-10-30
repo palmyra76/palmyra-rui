231b6aa1160d52db97c7d077a8618a8d660390c4
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2023-10-30 17:54:46 +0530

    Form Renderer Changes

--- dist/palmyra/form/FormUtil.js
+++ dist/palmyra/form/FormUtil.js
@@ -6,40 +6,42 @@ const o = (e, t) => {
     var s = e.substring(0, r), n = e.substring(r + 1);
     return o(n, t[s]);
   }
-}, i = (e, t, r) => {
+}, c = (e, t, r) => {
   var s = e.indexOf(".");
   if (s < 0) {
     t[e] = r;
     return;
   }
   var n = e.substring(0, s), u = e.substring(s + 1);
-  return (t[n] === void 0 || t[n] == null) && (t[n] = {}), i(u, t[n], r);
+  return (t[n] === void 0 || t[n] == null) && (t[n] = {}), c(u, t[n], r);
 }, b = (e, t) => {
   const r = o(e.attribute, t);
-  return c(e, r);
-}, c = (e, t) => {
+  return i(e, r);
+}, i = (e, t) => {
   switch (e.type || "string") {
     case "date":
       return g(t);
     case "radio":
     case "select":
     case "checkbox":
+    case "switch":
       return p(t, e);
     case "datetime":
       return y(t);
     case "serverlookup":
-      return a(t);
+      return x(t);
     case "textarea":
-    default:
       return l(t);
+    default:
+      return a(t);
   }
-}, g = (e, t) => e, y = (e, t) => e, l = (e, t) => e, p = (e, t) => {
+}, g = (e, t) => e, y = (e, t) => e, a = (e, t) => e, l = (e, t) => e, p = (e, t) => {
   var r = t.options;
   return r[e];
-}, a = (e, t) => e;
+}, x = (e, t) => e;
 export {
-  c as formatValue,
+  i as formatValue,
   b as getDisplayValue,
   o as getValueByKey,
-  i as setValueByKey
+  c as setValueByKey
 };
--- dist/palmyra/layout/flexiLayout/FormEditRenderer.js
+++ dist/palmyra/layout/flexiLayout/FormEditRenderer.js
@@ -1,27 +1,27 @@
 import { jsx as a } from "react/jsx-runtime";
-import { forwardRef as u, useRef as F, useMemo as h } from "react";
+import { forwardRef as p, useRef as F, useMemo as h } from "react";
 import g from "../container/FieldContainer.js";
 import x from "../../form/FieldGenerator.js";
-const M = u(function(t, w) {
+const M = p(function(t, C) {
   var n;
-  v(t);
-  const { formLayout: r, context: i } = t, { getFieldManager: m, formData: s } = i, f = r.Container || g;
+  w(t);
+  const { formLayout: r, context: i } = t, { getFieldManager: m, formData: f } = i, s = r.Container || g;
   F({});
-  const c = h(() => (e) => x(e, m), [s.data]);
-  var l = ((n = r.options) == null ? void 0 : n.columns) || 1, d = { columns: l };
-  return /* @__PURE__ */ a("div", { className: "palmyra-form-field-container-wrapper", children: r.fields.map((e, p) => /* @__PURE__ */ a(
-    f,
+  const l = h(() => (e) => x(e, m), [f.data]);
+  var c = ((n = r.options) == null ? void 0 : n.columns) || 1, d = { columns: c };
+  return /* @__PURE__ */ a("form", { className: "palmyra-form-field-container-wrapper", noValidate: !0, children: r.fields.map((e, u) => /* @__PURE__ */ a(
+    s,
     {
-      index: p,
+      index: u,
       field: e,
       label: e.title,
       options: d,
-      children: c(e)
+      children: l(e)
     },
     e.attribute
   )) });
 });
-function v(o) {
+function w(o) {
   var t;
   if (!((t = o.context) != null && t.getFieldManager))
     throw new Error('Form Context not available, if this the form page, set the type as "form" in the definition');
