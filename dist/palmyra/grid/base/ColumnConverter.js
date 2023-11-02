2a172b2ab4bd26b2f9c1004bb43db2986cacf37d
Raja K <ksvraja@gmail.com>
2023-11-02 15:40:37 +0530

    dist: build changes pushed

--- dist/palmyra/grid/base/ColumnConverter.js
+++ dist/palmyra/grid/base/ColumnConverter.js
@@ -1,27 +1,33 @@
 import { jsx as i } from "react/jsx-runtime";
 import { createColumnHelper as d } from "@tanstack/react-table";
 import { getFormatFn as l } from "./CellFormatter.js";
-const o = d();
-function g(e) {
+import { getValueByKey as u } from "../../form/FormUtil.js";
+import { hasDot as m } from "../../utils/StringUtil.js";
+const n = d();
+function b(e) {
   return e.map((r) => p(r));
 }
 function p(e) {
   var r = e.cellRenderer;
   let t = c(e);
   if (r)
-    return o.display({
-      id: n(e),
+    return n.display({
+      id: o(e),
       ...t,
       cell: r
     });
   let a = l(e);
-  return o.accessor(n(e), {
-    id: n(e),
+  return n.accessor(s(e), {
+    id: o(e),
     ...t,
     ...a
   });
 }
-function n(e) {
+function s(e) {
+  var r = e.attribute ? e.attribute : e.name;
+  return m(r) ? (t) => u(r, t) : r;
+}
+function o(e) {
   return e.name ? e.name : e.attribute;
 }
 function c(e) {
@@ -32,5 +38,5 @@ function c(e) {
   };
 }
 export {
-  g as generateColumns
+  b as generateColumns
 };
