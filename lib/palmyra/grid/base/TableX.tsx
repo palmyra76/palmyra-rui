f082aa8b61fc34552751b2cbd21705ac47deb6d2
surya-ponmani <gsuryaprakash2000@gmail.com>
2023-11-10 13:31:23 +0530

    Grid - LoadingIcon Set

--- lib/palmyra/grid/base/TableX.tsx
+++ lib/palmyra/grid/base/TableX.tsx
@@ -2,7 +2,7 @@
  * Basic structure to draw the table
  */
 import { useEffect, useState } from 'react'
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { CircularProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import { Paper, Box } from '@mui/material';
 import './Grid.css';
 import ColumnHeader from './ColumnHeader'
@@ -41,7 +41,7 @@ export default function TableX({ columnDefs, rowData, onRowClick, onRowStyle, on
     setSortColumn(sort);
   }
   const onSort = onSortColumn ? onSortChange : undefined;
-  
+
   return (
     <Box sx={{ width: '100%' }}>
       <TableContainer component={Paper}>
@@ -68,33 +68,36 @@ export default function TableX({ columnDefs, rowData, onRowClick, onRowStyle, on
               </TableRow>
             ))}
           </TableHead>
-          
-          {(null == rowData || undefined == rowData || 0 == rowData.length)? (<></>) : (
-          <TableBody >
-            {
-              table.getRowModel().rows
-                .map(row => {
-                  const rowStyle = onRowStyle(row.original);
-                  return (
-                    <TableRow key={row.id} className='table-row' >
-                      {row.getVisibleCells().map(cell => {
-                        return (
-                          <TableCell key={cell.id} style={rowStyle} onClick={() => onRowClick(row.original)}>
-                            {flexRender(
-                              cell.column.columnDef.cell,
-                              cell.getContext()
-                            )}
 
-                          </TableCell>
-                        )
-                      })}
-                    </TableRow>
-                  )
-                })}
-          </TableBody> )}
+          {(null == rowData || undefined == rowData || 0 == rowData.length) ? (<></>) : (
+            <TableBody >
+              {
+                table.getRowModel().rows
+                  .map(row => {
+                    const rowStyle = onRowStyle(row.original);
+                    return (
+                      <TableRow key={row.id} className='table-row' >
+                        {row.getVisibleCells().map(cell => {
+                          return (
+                            <TableCell key={cell.id} style={rowStyle} onClick={() => onRowClick(row.original)}>
+                              {flexRender(
+                                cell.column.columnDef.cell,
+                                cell.getContext()
+                              )}
+
+                            </TableCell>
+                          )
+                        })}
+                      </TableRow>
+                    )
+                  })}
+            </TableBody>)}
         </Table>
-        {(null == rowData)? (<div>Loading Data</div>) : 
-          ( undefined == rowData) ? (<div>Error while loading data</div>) : (0 == rowData.length)? (<EmptyChild/>) : (<></>)}
+        {(null == rowData) ? (<div>
+          Loading Data
+          <CircularProgress size={25} thickness={4} />
+        </div>) :
+          (undefined == rowData) ? (<div>Error while loading data</div>) : (0 == rowData.length) ? (<EmptyChild />) : (<></>)}
       </TableContainer>
     </Box>
   )
