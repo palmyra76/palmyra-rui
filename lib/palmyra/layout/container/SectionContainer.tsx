e8109b3751646709adecd269694f0fd94efaefdf
surya-ponmani <gsuryaprakash2000@gmail.com>
2023-11-21 09:44:18 +0530

    Accordion Set in Section Layout

--- lib/palmyra/layout/container/SectionContainer.tsx
+++ lib/palmyra/layout/container/SectionContainer.tsx
@@ -1,18 +1,30 @@
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import { SectionContainerInput } from "../flexiLayout/Types";
 import './SectionLayout.css';
+import { ExpandMore } from "@mui/icons-material";
+import { useState } from 'react';
 
 const SectionContainer = (props: SectionContainerInput) => {
     const title = props.title;
     const hideTitle = props.hideTitle || false;
+    const [expanded, setExpanded] = useState(true);
 
-    return (<div className='section-container'>
-        {(title && !hideTitle) ? (
-            <div className="palmyra-form-section-header">{title}</div>) : ''
-        }
-        <div className='section-container-child'>
-            {props.children}
+    const toggleExpand = () => {
+        setExpanded(!expanded);
+    }
+
+    return <>
+        <div className='section-container'>
+            <Accordion expanded={expanded} onChange={toggleExpand}>
+                <AccordionSummary className="palmyra-form-section-header-container" expandIcon={<ExpandMore />}>{(title && !hideTitle) ? (
+                    <div>{title}</div>) : ''
+                }</AccordionSummary>
+                <AccordionDetails>
+                    {props.children}
+                </AccordionDetails>
+            </Accordion>
         </div>
-    </div>)
+    </>
 }
 
 export default SectionContainer;
