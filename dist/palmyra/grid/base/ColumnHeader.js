d4016da78f8627c6d94d690dd273cce996f6bb47
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2024-01-29 15:21:23 +0530

    latest build

--- dist/palmyra/grid/base/ColumnHeader.js
+++ dist/palmyra/grid/base/ColumnHeader.js
@@ -2,7 +2,7 @@ import { jsx as e, jsxs as p } from "react/jsx-runtime";
 import { useState as b } from "react";
 import { ArrowUpward as x, ArrowDownward as y } from "@mui/icons-material";
 import { TableCell as S } from "@mui/material";
-const v = ({ header: o, children: a, onSortChange: l, onHeaderStyle: i }) => {
+const v = ({ header: o, children: i, onSortChange: l, onHeaderStyle: a }) => {
   var c;
   const [r, m] = b(""), u = ((c = o.column.columnDef.meta) == null ? void 0 : c.attribute) || o.id, n = !o.column.columnDef.enableSorting, f = () => {
     if (!(l === void 0 || n)) {
@@ -21,15 +21,16 @@ const v = ({ header: o, children: a, onSortChange: l, onHeaderStyle: i }) => {
       m(t), l(u, t);
     }
   };
-  var s = i(o.original);
+  var s = a(o.original);
   n || (s.cursor = "pointer");
   const d = o.column.columnDef.meta;
   return /* @__PURE__ */ e(S, { colSpan: o.colSpan, style: s, children: /* @__PURE__ */ p("div", { style: {
     display: "flex",
+    fontWeight: "bold",
     gap: "2px",
     justifyContent: d.columnDef.type === "number" ? "flex-end" : "flex-start"
   }, onClick: () => f(), children: [
-    a,
+    i,
     r === "asc" ? /* @__PURE__ */ e(x, { style: { fontSize: "22px", color: "rgb(0,0,0,0.5)" } }) : r === "desc" ? /* @__PURE__ */ e(y, { style: { fontSize: "22px", color: "rgb(0,0,0,0.5)" } }) : null
   ] }) }, o.id);
 };
