3b431739d8736af70ee6f23c75161aa7b76f62e5
Raja K <ksvraja@gmail.com>
2024-04-11 09:45:51 +0530

    Grouped Bar converter - style issue fixed

--- dist/palmyra/chart/chartjs/style/GroupedBarConverter.js
+++ dist/palmyra/chart/chartjs/style/GroupedBarConverter.js
@@ -1,12 +1,18 @@
-import { ArrayStyleConverterFactory as r } from "./base/ArrayStyleConverterFactory.js";
-import { DatasetStyleConverterFactory as o } from "./base/DatasetStyleConverterFactory.js";
-import { RandomStyleConverterFactory as t, NoopStyleConverterFactory as e } from "./base/RandomStyleConverterFactory.js";
-const n = {
-  Array: r,
-  Named: o,
-  Random: t,
-  Noop: e
+import { RandomStyleConverterFactory as s, NoopStyleConverterFactory as y } from "./base/RandomStyleConverterFactory.js";
+import { assignStyles as u } from "./util.js";
+function a(o, t, r, n) {
+  const e = n ? n[r] : "";
+  return o[e] || o[t.label] || o[t.key] || o[r];
+}
+const l = (o, t) => (r, n) => (r == null || r == null || r.datasets.map((e, c) => {
+  const m = a(o, e, c, r.labels);
+  u(e, m);
+}), r), v = {
+  Array: l,
+  Named: l,
+  Random: s,
+  Noop: y
 };
 export {
-  n as default
+  v as default
 };
--- lib/palmyra/chart/chartjs/style/GroupedBarConverter.ts
+++ lib/palmyra/chart/chartjs/style/GroupedBarConverter.ts
@@ -1,11 +1,30 @@
-import { IStyleConverterFactory, StyleType } from "../..";
-import { ArrayStyleConverterFactory } from "./base/ArrayStyleConverterFactory";
-import { DatasetStyleConverterFactory } from "./base/DatasetStyleConverterFactory";
+import { ChartStyle, DataSetType, DataSets, IStyleConverterFactory, ITransformOptions, StyleOptions, StyleType } from "../..";
 import { NoopStyleConverterFactory, RandomStyleConverterFactory } from "./base/RandomStyleConverterFactory";
+import { assignStyles } from "./util";
 
+
+function getStyle(styleOptions: StyleOptions, dataset, index, labels): ChartStyle {
+    const label = labels ? labels[index] : '';
+    return styleOptions[label] || styleOptions[dataset.label] || styleOptions[dataset.key] || styleOptions[index];
+}
+
+
+const DatasetStyleConverterFactory: IStyleConverterFactory = (styleOptions: StyleOptions,
+    transformOptions?: ITransformOptions) => {
+    return (data: DataSets<DataSetType>, options?: any): DataSets<DataSetType> => {
+        if (null == data || undefined == data)
+            return data;
+
+        data.datasets.map((d, index) => {
+            const style = getStyle(styleOptions, d, index, data.labels)
+            assignStyles(d, style);
+        });
+        return data;
+    }
+}
 
 const converters: Partial<Record<StyleType, IStyleConverterFactory>> = {
-    "Array": ArrayStyleConverterFactory,
+    "Array": DatasetStyleConverterFactory,
     "Named": DatasetStyleConverterFactory,
     "Random": RandomStyleConverterFactory,
     "Noop": NoopStyleConverterFactory
